d9be083df281507b44b9f3723d46a13321377619
James Fisher <jfisher396@hotmail.com>
2020-07-12 12:19:14 -0700

    linked html pages to get requests

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 // ================================================================================
 
 // require("./routes/apiRoutes")(app);
-// require("./routes/htmlRoutes")(app);
+require("./routes/htmlRoutes.js")(app);
 
 // =============================================================================
 // LISTENER
